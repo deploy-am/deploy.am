1ef61a2b02fe911957a2fdcba2b85c29784413fe
inductor <kela@inductor.me>
2018-08-18 15:41:41 +0900

    Change top message

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <TopMessage msg="Welcome to Your Vue.js App"/>
+    <TopMessage msg="Deploy.am Podcast"/>
   </div>
 </template>
 
