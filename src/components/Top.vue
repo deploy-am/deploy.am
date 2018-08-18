5abcaa4e33381ebb27a064913fc8136f3308178b
inductor <kela@inductor.me>
2018-08-18 12:51:34 +0900

    modify stuff

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -33,7 +33,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'TopMessage',
   props: {
     msg: String
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TopMessage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TopMessage from '@/components/Top.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    TopMessage
   }
 }
 </script>
