0422117f09f466e29d35e5b3f5767127f055c86e
inductor <kela@inductor.me>
2018-08-18 20:18:58 +0900

    modify router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Podcast from '@/views/Podcast.vue'
 
 Vue.use(Router)
 
@@ -14,17 +16,12 @@ export default new Router({
             component: Home
         },
         {
-            path: '/index.html',
-            name: 'home',
-            component: Home
-        },
-        {
             path: '/about',
             name: 'about',
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+            component: About
         },
         {
             path: '/podcast',
@@ -32,7 +29,7 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/Podcast.vue')
+            component: Podcast
         }
     ]
 })
