4a89c2dcc529080a805c79ac986538ca5f808b86
inductor <kela@inductor.me>
2018-08-18 20:20:31 +0900

    Add podcast index

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
+import PodcastIndex from '@/views/PodcastIndex.vue'
 import Podcast from '@/views/Podcast.vue'
 
 Vue.use(Router)
@@ -25,6 +26,14 @@ export default new Router({
         },
         {
             path: '/podcast',
+            name: 'podcast',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: PodcastIndex
+        },
+        {
+            path: '/podcast/:id',
             name: 'podcast',
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
