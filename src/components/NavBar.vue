b7ddd8eb6957fdf3ffa80ab1ac74b891231e999c
inductor <kela@inductor.me>
2018-08-19 11:55:16 +0900

    remove script from navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -36,19 +36,3 @@
         }
     }
 </style>
-
-<script>
-    export default {
-        data() {
-            return {
-                activeIndex: '1',
-                activeIndex2: '1'
-            };
-        },
-        methods: {
-            handleSelect(key, keyPath) {
-                console.log(key, keyPath);
-            }
-        }
-    }
-</script>
