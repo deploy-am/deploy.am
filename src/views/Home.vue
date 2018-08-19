573414a2190b980d134b1785dd413705a1211fe0
inductor <kela@inductor.me>
2018-08-19 19:39:41 +0900

    fix abst

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="disctiption">
         <content>
-            <p>Deploy.amは、技術系Podcastの中でも開発をあまりしない</p>
+            <h2>What's Deploy.am?</h2>
+            <p>Deploy.amは、「技術系Podcast」の中でも、開発をあまりしない</p>
             <li>インフラ(構築やら運用やら)</li>
             <li>運用(情シスやら社内SEやら)</li>
             <li>サポートエンジニア</li>
