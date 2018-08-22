f462628f0c4d2ade44dd6a21b5e8c26f5b8df15a
inductor <kela@inductor.me>
2018-08-22 10:41:22 +0900

    add webfont

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,9 @@
 </template>
 
 <style lang="scss">
+    @import url('https://fonts.googleapis.com/css?family=M+PLUS+1p&subset=japanese');
     #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-family: 'M PLUS 1p', 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
