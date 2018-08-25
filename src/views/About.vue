018a2ad4f9d2da000b73bc8df3dbf1232678aa7b
inductor <kela@inductor.me>
2018-08-25 15:44:12 +0900

    fix self introduction

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,25 +4,29 @@
             <h2>Presenters</h2>
             <el-card class="box-card">
                 <div slot="header" class="clearfix">
-                    <span class="podcast-title"><a href="https://twitter.com/hiyoko_taisa"
-                                                   target="_blank">ひよこ大佐 <font-awesome-icon :icon="['fab', 'twitter']"
+                    <img alt="hiyokotaisa logo" src="../assets/hiyoko_taisa_logo.jpg" width="30" height="30">
+                    <span class="podcast-title"> ひよこ大佐 <a href="https://twitter.com/hiyoko_taisa"
+                                                   target="_blank"><font-awesome-icon :icon="['fab', 'twitter']"
                                                                                             class="icon alt"/></a></span>
                     <!--<span class="podcast-subtitle"></span>-->
                 </div>
                 <div class="text item">
-                    <p>じこしょうかい</p>
+                    <p>OSSの会社でAnsibleにかかわるおしごとをしています。</p>
+                    <p>このアカウントは個人の主張であり、所属する組織の見解や方針などを代表するわけがありません。</p>
                 </div>
             </el-card>
             <el-card class="box-card">
                 <div slot="header" class="clearfix">
-                    <span class="podcast-title"><a
-                            href="https://twitter.com/_inductor_" target="_blank">inductor <font-awesome-icon
+                    <img alt="inductor logo" src="../assets/inductor_logo.jpg" width="30" height="30">
+                    <span class="podcast-title"> inductor <a
+                            href="https://twitter.com/_inductor_" target="_blank"><font-awesome-icon
                             :icon="['fab', 'twitter']" class="icon alt"/></a></span>
 
                     <!--<span class="podcast-subtitle"></span>-->
                 </div>
                 <div class="text item">
-                    <p>じこしょうかい</p>
+                    <p>とあるファッション系ECサイトの裏側でインフラをやったりDevOps的なことをやったりしています。</p>
+                    <p>コンテナや自動化に興味があります。</p>
                 </div>
             </el-card>
             <h2>このサイトについて</h2>
@@ -33,7 +37,7 @@
 </template>
 
 <style>
-    .about {
-        text-align: left;
-    }
+.about {
+  text-align: left;
+}
 </style>
