68598de97797c32222b765cad0c272bf6008bdbb
inductor <kela@inductor.me>
2018-09-25 11:34:46 +0900

    try ie-buster

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'ie-buster/dist/ie-buster.min.js'
 import App from './App.vue'
 import router from './router'
 import store from './store'
